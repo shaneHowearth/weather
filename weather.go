6effb0acc988a97c9e42ca2f66ec2984426ddac8
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-11-12 15:42:22 +1100

    refactor (providers): use anonymous struct for DTO

--- weather.go
+++ weather.go
@@ -1,8 +1,122 @@
-package melbweather
+package weather
 
-// WeatherProvider -
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+// Minimum time between requests
+const minGap = 3 * time.Second
+
+// Known cities
+var cities = map[string]struct{}{"melbourne": struct{}{}}
+
+// Provider -
 // All weather providers should implement this interface to allow them to be
 // used by the application.
-type WeatherProvider interface {
+type Provider interface {
 	GetWeather(city string) (struct{ Temperature, WindSpeed float64 }, error)
+}
+
+// Data -
+type Data struct {
+	m         sync.Mutex
+	providers []Provider
+	last      struct {
+		WindSpeed   float64 `json:"wind_speed"`
+		Temperature float64 `json:"temperature_degrees"`
+	}
+	touched time.Time
+}
+
+// NewData -
+func NewData(p []Provider) (*Data, error) {
+	// Must have at least one provider
+	if len(p) < 1 {
+		return nil, fmt.Errorf("must have at least one provider")
+	}
+	return &Data{
+		providers: p,
+		touched:   time.Now().Add(-100 * 24 * 365 * time.Hour), // Default the last touched to 100 years ago
+	}, nil
+}
+
+// AddProvider -
+func (d *Data) AddProvider(p Provider) error {
+	d.m.Lock()
+	defer d.m.Unlock()
+	d.providers = append(d.providers, p)
+	return nil
+}
+
+// Weather -
+func (d *Data) Weather(w http.ResponseWriter, r *http.Request) {
+	// only GET allowed
+	if r.Method != http.MethodGet {
+		http.Error(w, "Bad method - Go away!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// city is required
+	query := r.URL.Query()
+	city, ok := query["city"]
+	if !ok {
+		http.Error(w, "Bad Request -", http.StatusBadRequest)
+		return
+	}
+
+	// unknown city provided
+	if _, ok := cities[strings.ToLower(strings.TrimSpace(city[0]))]; !ok {
+		http.Error(w, "Sorry, don't know that city", http.StatusBadRequest)
+		return
+	}
+
+	d.m.Lock()
+	defer d.m.Unlock()
+	// Rate limit
+	// Note this limit is on this endpoint rather than provider specific
+	if time.Now().Sub(d.touched) < minGap {
+		// use the cached value
+		if resp, err := json.Marshal(d.last); err != nil {
+			_, err = w.Write(resp)
+			if err != nil {
+				log.Printf("unable to write cached %#v in GetWeather handler with error %v", d.last, err)
+			}
+		} else {
+			log.Printf("unable to marshal cached %#v, with error %v", d.last, err)
+		}
+		return
+	}
+
+	// try each of the providers
+	for i := range d.providers {
+		val, err := d.providers[i].GetWeather(city[0])
+		if err != nil {
+			// log the error
+			log.Printf("ERROR %v", err)
+			continue
+		}
+
+		// Update cache
+		d.touched = time.Now()
+		d.last.Temperature = val.Temperature
+		d.last.WindSpeed = val.WindSpeed
+
+		// no need to try any more providers
+		break
+	}
+
+	if resp, err := json.Marshal(d.last); err == nil {
+		_, err = w.Write(resp)
+		if err != nil {
+			log.Printf("unable to write %#v in GetWeather handler with error %v", d.last, err)
+		}
+	} else {
+		log.Printf("unable to marshal %#v, with error %v", d.last, err)
+	}
 }
